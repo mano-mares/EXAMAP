4637e741cbf279b6294ae117889d2fb191fec161
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-02 18:59:25 +0200

    Add subject text field

--- lib/create_exam.dart
+++ lib/create_exam.dart
@@ -8,11 +8,30 @@ class CreateExamPage extends StatefulWidget {
 }
 
 class _CreateExamPageState extends State<CreateExamPage> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Examen maken')),
-      body: const Text('Create exam page'),
-    );
+        appBar: AppBar(
+          title: const Text('Examen maken'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Subject*',
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ]),
+          ),
+        ));
   }
 }
