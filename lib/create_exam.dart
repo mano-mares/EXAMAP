b8662543942f649fea5bc8e708823ce6267f4cea
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-02 19:21:10 +0200

    Add time picker field

--- lib/create_exam.dart
+++ lib/create_exam.dart
@@ -12,6 +12,37 @@ class _CreateExamPageState extends State<CreateExamPage> {
   // and allows validation of the form.
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  final TimeOfDay _initialTime = const TimeOfDay(hour: 1, minute: 30);
+  late TextEditingController _timeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _timeController = TextEditingController(text: _parseTime(_initialTime));
+  }
+
+  void _selectTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+        context: context,
+        initialTime: _initialTime,
+        initialEntryMode: TimePickerEntryMode.input,
+        builder: (BuildContext context, Widget? child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+            child: child!,
+          );
+        });
+    if (newTime != null) {
+      setState(() {
+        _timeController.text = _parseTime(newTime);
+      });
+    }
+  }
+
+  String _parseTime(TimeOfDay time) {
+    return '${time.hour}:${time.minute}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +57,19 @@ class _CreateExamPageState extends State<CreateExamPage> {
             child: Column(children: [
               TextFormField(
                 decoration: const InputDecoration(
-                  labelText: 'Subject*',
+                  labelText: 'Vak',
                   border: OutlineInputBorder(),
                 ),
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _timeController,
+                decoration: const InputDecoration(
+                  labelText: 'Duur',
+                  border: OutlineInputBorder(),
+                ),
+                readOnly: true,
+                onTap: _selectTime,
               )
             ]),
           ),
