38619b700bb34f114af6f6746265d856b12c6457
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-02 15:18:26 +0200

    Create exam page

---
+++ lib/create_exam.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CreateExamPage extends StatefulWidget {
+  const CreateExamPage({Key? key}) : super(key: key);
+
+  @override
+  State<CreateExamPage> createState() => _CreateExamPageState();
+}
+
+class _CreateExamPageState extends State<CreateExamPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Examen maken')),
+      body: const Text('Create exam page'),
+    );
+  }
+}
