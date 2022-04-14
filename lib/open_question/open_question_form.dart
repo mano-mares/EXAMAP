a11a9de37abc058770918104fad3e08b22ff1954
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-14 15:33:48 +0200

    Add textfield and button

--- lib/open_question/open_question_form.dart
+++ lib/open_question/open_question_form.dart
@@ -9,26 +9,67 @@ class OpenQuestionForm extends StatefulWidget {
 }
 
 class _OpenQuestionFormState extends State<OpenQuestionForm> {
+  late TextEditingController _questionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _questionController = TextEditingController();
+  }
+
+  void _addOpenQuestion() {
+    // TODO: add question to questions list in exam object.
+
+    // TODO: go back to create exam form.
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
-        children: const [
-          Text(
+        children: [
+          const Text(
             strings.title,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 50.0,
             ),
           ),
-          Text(
+          const Text(
             strings.description,
             style: TextStyle(
               fontSize: 20.0,
             ),
           ),
+          const SizedBox(
+            height: 16.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              controller: _questionController,
+              decoration: const InputDecoration(
+                labelText: strings.labelQuestion,
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _addOpenQuestion,
+            child: const Text(strings.buttonText),
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(20.0),
+              textStyle: const TextStyle(fontSize: 16.0),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _questionController.dispose();
+    super.dispose();
   }
 }
--- lib/open_question/strings.dart
+++ lib/open_question/strings.dart
@@ -2,3 +2,5 @@ const String appBarTitle = 'Vraag toevoegen';
 // Open Question Form
 const String title = 'Open vraag';
 const String description = 'Voeg een open vraag toe aan het examen.';
+const String labelQuestion = 'Vraag';
+const String buttonText = 'Toevoegen';
