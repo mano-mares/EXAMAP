c397fb55b6dd13420389ab5da3db04901b3d7f50
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-17 17:22:08 +0200

    Add title

--- lib/add_question/code_correction/code_correction_form.dart
+++ lib/add_question/code_correction/code_correction_form.dart
@@ -9,12 +9,40 @@ class CodeCorrectionForm extends StatefulWidget {
 }
 
 class _CodeCorrectionFormState extends State<CodeCorrectionForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(strings.appBarTitle),
         centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(children: [
+              const Text(
+                strings.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50.0,
+                ),
+              ),
+              const Text(
+                strings.description,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              )
+            ]),
+          ),
+        ),
       ),
     );
   }
