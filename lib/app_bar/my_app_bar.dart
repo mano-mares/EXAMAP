12c6d2ee8824a49dbd9c2da7ba20bbb87651fd2d
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-25 21:31:25 +0200

    Extract pop up menu as widget

--- lib/app_bar/my_app_bar.dart
+++ lib/app_bar/my_app_bar.dart
@@ -1,15 +1,9 @@
-import 'package:examap/main.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../authentication/popup_menu_button_logout.dart';
 import '../homepage_docent/strings.dart' as strings;
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({Key? key}) : super(key: key);
-
-  Future<void> _signOut() async {
-    await FirebaseAuth.instance.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,27 +11,8 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         title: const Text(strings.appBarTitle),
         centerTitle: true,
         automaticallyImplyLeading: false,
-        actions: [
-          PopupMenuButton<Text>(
-            onSelected: (value) {
-              _signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const MyApp()),
-              );
-            },
-            icon: const Icon(Icons.more_vert),
-            itemBuilder: (context) {
-              return [
-                const PopupMenuItem(
-                  value: Text('signout'),
-                  child: Text(
-                    strings.logOutAppBarPopupMenuItemTxt,
-                  ),
-                ),
-              ];
-            },
-          )
+        actions: const [
+          PopupMenuButtonLogout(),
         ],
       ),
     );
