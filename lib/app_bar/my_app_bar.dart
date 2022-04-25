71c339927d59c37fb089b5a9156c2759bd6e7e63
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-04-25 21:25:47 +0200

    Log out functionality

--- lib/app_bar/my_app_bar.dart
+++ lib/app_bar/my_app_bar.dart
@@ -1,8 +1,14 @@
+import 'package:examap/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../homepage_docent/strings.dart' as strings;
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({Key? key}) : super(key: key);
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +19,18 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         automaticallyImplyLeading: false,
         actions: [
           PopupMenuButton<Text>(
+            onSelected: (value) {
+              _signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyApp()),
+              );
+            },
             icon: const Icon(Icons.more_vert),
             itemBuilder: (context) {
               return [
                 const PopupMenuItem(
+                  value: Text('signout'),
                   child: Text(
                     strings.logOutAppBarPopupMenuItemTxt,
                   ),
