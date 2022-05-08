8db24e5e83c4292b15cbcf70fe70aa2953fc238f
jelle <s116048@ap.be>
2022-05-08 18:20:54 +0200

    Add,Delete and load students from FireBase; StudentList, AddStudent pages

--- lib/student_list/student_list.dart
+++ lib/student_list/student_list.dart
@@ -1,9 +1,14 @@
 import 'dart:ui';
+import 'package:examap/student_list/add_student/add_student.dart';
 import 'package:flutter/material.dart';
 
 import './strings.dart' as strings;
 import '../Student/choose_student_number/students.dart' as students;
 import '../res/style/my_fontsize.dart' as sizes;
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:examap/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StudentList extends StatefulWidget {
   const StudentList({Key? key}) : super(key: key);
@@ -15,15 +20,43 @@ class StudentList extends StatefulWidget {
 class _StudentListState extends State<StudentList> {
   TextEditingController studentController = TextEditingController();
 
-  void _addStudent() {
-    students.studentList = [];
-    List<String> rawStudList = studentController.text.split(",");
+  FirebaseFirestore? firestore;
 
-    for (var student in rawStudList) {
-      students.studentList.add(student.trim());
-      // print(student.trim());
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp(options: DefaultFirebaseOptions.android);
+  }
+
+  @override
+  void dispose() {
+    studentController.dispose();
+    super.dispose();
+  }
+
+  Future<void> getData(studentStore) async {
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await studentStore.get();
+    // Get data from docs and convert map to List
+    final allData = querySnapshot.docs.map((doc) => doc.id).toList().join(", ");
+    print(allData);
+
+    studentController.text = allData.toString();
+  }
+
+  void _loadStudents() async {
+    firestore = FirebaseFirestore.instance;
+
+    if (firestore != null) {
+      // TO DO
+      // Reference the correct Exam
+      CollectionReference studentStore = firestore!
+          .collection("exam2")
+          .doc("Python Development")
+          .collection("students");
+
+      await getData(studentStore);
     }
-    print(students.studentList);
   }
 
   @override
@@ -34,29 +67,47 @@ class _StudentListState extends State<StudentList> {
         Padding(
             padding: const EdgeInsets.only(
                 left: 128, right: 128, top: 128, bottom: 50),
+            // Read only field...
             child: TextFormField(
+              enabled: false,
               style: const TextStyle(fontSize: sizes.text),
               controller: studentController,
               restorationId: 'student_list_field',
               // focusNode: _lifeStory,
               decoration: const InputDecoration(
-                helperStyle: TextStyle(fontSize: sizes.xSmall),
-                border: OutlineInputBorder(),
-                hintText: strings.hintText,
-                helperText: strings.helperText,
-                labelText: strings.hintText,
-              ),
+                  helperStyle: TextStyle(fontSize: sizes.xSmall),
+                  border: OutlineInputBorder()),
               maxLines: 15,
             )),
-        ElevatedButton(
-          onPressed: () => _addStudent(),
-          child: const Text(strings.addStudent,
-              style: TextStyle(fontSize: sizes.btnSmall)),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.red,
-            fixedSize: const Size(150, 50),
-          ),
-        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                onPressed: () => _loadStudents(),
+                child: const Text(strings.loadStudent,
+                    style: TextStyle(fontSize: sizes.btnSmall)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  fixedSize: const Size(165, 50),
+                )),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const StudentListAdd(),
+                  ),
+                );
+              },
+              child: const Text(strings.editStudentList,
+                  style: TextStyle(fontSize: sizes.btnSmall)),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                fixedSize: const Size(155, 50),
+              ),
+            ),
+          ],
+        )
       ]),
     );
   }
