825e67f4cf15f7779c6a7d8af7c24ad4d7a0862a
Mano Mares <mano.mares@hotmail.be>
2022-05-11 10:32:04 +0200

    Add UI Details

--- lib/correct_exam/choose_student_correction.dart
+++ lib/correct_exam/choose_student_correction.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../res/style/my_fontsize.dart' as sizes;
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChooseStudentCorrection extends StatefulWidget {
   const ChooseStudentCorrection({Key? key}) : super(key: key);
@@ -9,6 +12,18 @@ class ChooseStudentCorrection extends StatefulWidget {
 }
 
 class _ChooseStudentCorrection extends State<ChooseStudentCorrection> {
+  late FirebaseFirestore firestore;
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp(options: DefaultFirebaseOptions.android);
+  }
+
+  void loadFirestore() {
+    firestore = FirebaseFirestore.instance;
+  }
+
   void chooseStudent() {
     print("Choose student");
   }
@@ -42,7 +57,7 @@ class _ChooseStudentCorrection extends State<ChooseStudentCorrection> {
               ),
             ),
             const Padding(
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
               child: Text(
                 "Studenten",
                 style: TextStyle(
@@ -56,36 +71,134 @@ class _ChooseStudentCorrection extends State<ChooseStudentCorrection> {
                 fontSize: sizes.small,
               ),
             ),
-            Container(
-              color: Colors.grey,
-              child: Column(
-                children: [
-                  const Text(
-                    "Niet verbeterd",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        fontSize: sizes.medium, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 60, 20, 40),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(80, 30, 80, 30),
-                    child: SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                          itemCount: items.length,
-                          itemBuilder: (context, index) {
-                            final item = items[index];
-                            return Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(item),
-                                Text("Punten"),
-                                Text("Klik hier")
-                              ],
-                            );
-                          }),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(80, 20, 0, 0),
+                          child: Text(
+                            "Niet verbeterd",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: sizes.medium,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(""),
+                        Text("")
+                      ],
                     ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(80, 30, 80, 30),
+                      child: SizedBox(
+                        height: 200,
+                        child: ListView.separated(
+                            separatorBuilder: (context, index) =>
+                                const Divider(color: Colors.black26),
+                            itemCount: items.length,
+                            itemBuilder: (context, index) {
+                              final item = items[index];
+                              return Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    item,
+                                    style:
+                                        const TextStyle(fontSize: sizes.xSmall),
+                                  ),
+                                  const Text(
+                                    "Punten",
+                                    style: TextStyle(
+                                        color: Colors.black38,
+                                        fontSize: sizes.xSmall),
+                                  ),
+                                  InkWell(
+                                    child: const Icon(Icons.arrow_forward_ios),
+                                    onTap: () {
+                                      // TODO: Go to student correct exam page
+                                    },
+                                  ),
+                                ],
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
                   ),
-                ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(80, 20, 0, 0),
+                          child: Text(
+                            "Verbeterd",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: sizes.medium,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(""),
+                        Text("")
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(80, 30, 80, 30),
+                      child: SizedBox(
+                        height: 200,
+                        child: ListView.separated(
+                            separatorBuilder: (context, index) =>
+                                const Divider(color: Colors.black26),
+                            itemCount: items.length,
+                            itemBuilder: (context, index) {
+                              final item = items[index];
+                              return Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    item,
+                                    style:
+                                        const TextStyle(fontSize: sizes.xSmall),
+                                  ),
+                                  const Text(
+                                    "2/20",
+                                    style: TextStyle(
+                                      fontSize: sizes.xSmall,
+                                    ),
+                                  ),
+                                  // Check if isPassed
+                                  const Text("Geslaagd"),
+                                ],
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ]),
