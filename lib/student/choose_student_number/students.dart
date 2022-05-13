0642537906fd01b9caf06df4d1c66e37d0009e74
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2022-05-13 22:07:26 +0200

    query for student number

--- lib/student/choose_student_number/students.dart
+++ lib/student/choose_student_number/students.dart
@@ -1,3 +1,9 @@
-final List<String> studentNumbers = ["S107983", "S205348", "102875", 's113573'];
+final List<String> studentNumbers = [
+  "S107983",
+  "S205348",
+  "102875",
+  's113573',
+  's123456',
+];
 //get students from the list in this file
 List<String> studentList = [];
--- lib/student/student_exam/submit_exam_page.dart
+++ lib/student/student_exam/submit_exam_page.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
 import 'package:examap/main.dart';
 import 'package:examap/models/exam.dart';
 import 'package:examap/student/student_state.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:examap/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../res/style/my_fontsize.dart' as sizes;
 
@@ -21,10 +26,35 @@ class SubmitExamPage extends StatefulWidget {
 }
 
 class _SubmitExamPageState extends State<SubmitExamPage> {
-  _submitExam() {
+  late FirebaseFirestore firestore;
+
+  @override
+  void initState() {
+    Firebase.initializeApp(options: DefaultFirebaseOptions.android);
+    loadFirestore();
+  }
+
+  void loadFirestore() async {
+    firestore = FirebaseFirestore.instance;
+  }
+
+  _submitExam() async {
     // TODO: write answers to firestore.
-    String studentNumber = StudentState.studentNumber;
-    print("Write answers to doc ${studentNumber}...");
+    String studentNumber = StudentState.studentNumber.trim().toLowerCase();
+    //print("Write answers to doc ${studentNumber}...");
+
+    // Get student doc
+    final studentsRef = firestore
+        .collection('dummy_data_examap')
+        .doc('exam')
+        .collection('students')
+        .doc(studentNumber);
+
+    // Get studentdoc
+    final DocumentSnapshot studentDoc = await studentsRef.get();
+
+    print(studentDoc.id);
+    // Write answers to student doc
 
     // Go back to the main login page by replacing the page.
     Navigator.pushReplacement(
@@ -40,8 +70,8 @@ class _SubmitExamPageState extends State<SubmitExamPage> {
 
   @override
   Widget build(BuildContext context) {
-    print("ANSWERS OF THE STUDENT");
-    print(widget.answers);
+    //print("ANSWERS OF THE STUDENT");
+    //print(widget.answers);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Examen indienen'),
@@ -55,7 +85,7 @@ class _SubmitExamPageState extends State<SubmitExamPage> {
           child: Container(
             width: MediaQuery.of(context).size.width,
             child: ElevatedButton(
-              onPressed: () => _submitExam(),
+              onPressed: () async => await _submitExam(),
               child: const Text(
                 'Indienen',
                 style: TextStyle(fontSize: sizes.btnMedium),
