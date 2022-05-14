8b0d1a17dc50c2f62c75fd72ef484f9883c05b58
Mano Mares <mano.mares@hotmail.be>
2022-05-14 22:03:19 +0200

    Add choose student dynamic

--- lib/admin/correct_exam/correct_exam.dart
+++ lib/admin/correct_exam/correct_exam.dart
@@ -1,4 +1,3 @@
-import 'package:examap/student/choose_student_number/students.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
